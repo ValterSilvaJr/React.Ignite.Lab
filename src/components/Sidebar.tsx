b3fae99f7b2efbf1efd8c16db75e98601a9c1a83
Valter Silva <valtersilvajr@live.com>
2022-06-25 14:31:52 -0300

    chore: codegen for graphql

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    slug
-    title
-    lessonType
-    availableAt
-  }
-}
-`
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: 'live' | 'class';
-  }[]
-}
 
 export function Sidebar(){
   const navigate = useNavigate();
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+  const { data } = useGetLessonsQuery()
   const { slug = data?.lessons[0].slug } = useParams();
   
   useEffect(() => {
